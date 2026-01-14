e42be30ea9312c0f8121161ac272fd464600591d
jmchargue54 <jmchargue54@users.noreply.github.com>
2026-01-14 11:00:30 -0700

    startup

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
+    extends: ['js/recommended'],
   },
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     languageOptions: { globals: globals.browser },
   },
   {
     rules: {
-      "no-unused-vars": "warn",
-      "no-undef": "warn",
-      quotes: ["error", "double", { allowTemplateLiterals: true }],
-      "no-console": "warn",
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+      quotes: ['error', 'single', { allowTemplateLiterals: true }],
+      'no-console': 'warn',
     },
   },
 ]);
